da498c055331c589356fd526c10cca1fa8566804
xodaaaa <elmostwanted32@gmail.com>
2020-07-05 17:16:10 -0400

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -26,7 +26,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),beryllium)
+ifeq ($(TARGET_DEVICE),polaris)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
